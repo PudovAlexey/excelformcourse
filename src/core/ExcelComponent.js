f4da4527cfba1c3dbd5896cc71ca82e66d1c9733
Alex <pudo-aleksej@yandex.ru>
2021-09-25 15:55:28 +0300

    add resize rows

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,11 +1,12 @@
-/* eslint linebreak-style: ["error", "windows"]*/
-import {DomListener} from './DomListener';
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
   }
+
+  // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
